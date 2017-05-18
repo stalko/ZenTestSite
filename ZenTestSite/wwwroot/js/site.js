b7a4ccf1b1c4b5d15f43228c0ac124455dc2d71c
Odnostalko <stalko23@gmail.com>
2017-05-18 21:45:56 +0300

    - Создал проект - Добавил HTML-страницы

---
+++ ZenTestSite/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Write your Javascript code.
+$(document).ready(function () {
+      //called when key is pressed in textbox
+      $("#ageNumber").keypress(function (e) {
+         //if the letter is not digit then display error and don't type anything
+         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            //display error message
+            $("#errmsg").html("Digits Only").show().fadeOut("slow");
+                   return false;
+        }
+       });
+    });
